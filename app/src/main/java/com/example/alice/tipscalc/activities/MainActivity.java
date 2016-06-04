0529b55ebd3ebaf369857389230280dad1055d5b
Alicia Beltran <aliceresponde@gmail.com>
2016-06-03 20:43:38 -0500

    create  menu, and menu layout

--- app/src/main/java/com/example/alice/tipscalc/activities/MainActivity.java
+++ app/src/main/java/com/example/alice/tipscalc/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,57 +22,37 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Bind(R.id.inputBill)
-    EditText inputBill;
-    @Bind(R.id.btnSubmit)
-    Button btnSubmit;
-    @Bind(R.id.inputPrecentage)
-    EditText inputPrecentage;
-    @Bind(R.id.guideline)
-    Guideline guideline;
-    @Bind(R.id.btnIncrease)
-    Button btnIncrease;
-    @Bind(R.id.btnDecrese)
-    Button btnDecrese;
-    @Bind(R.id.separator)
-    View separator;
-    @Bind(R.id.txtTip)
-    TextView txtTip;
-    @Bind(R.id.btnClear)
-    Button btnClear;
-
     //instancia del listener
-    private TipHistoryListFragmentListener fragmentListener;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+        //link  UI and  views
 
         //Vincular el fragmento desde el support  fragment
-        TipHistoryListFragment fragment = (TipHistoryListFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragmentList);
-        fragment.setRetainInstance(true); //conserve su estado
-        fragmentListener = (TipHistoryListFragmentListener) fragment;
+
     }
 
 
 
 
-    @OnClick({R.id.btnSubmit, R.id.btnIncrease, R.id.btnDecrese, R.id.btnClear})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnSubmit:
-                break;
-            case R.id.btnIncrease:
-                break;
-            case R.id.btnDecrese:
-                break;
-            case R.id.btnClear:
-                break;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.action_about) {
+            about();
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about() {
     }
 }
